f97e43fe8162ae3ae439397290ce5e7262ca211f
Yano Andriyanto <yano@xendit.co>
2025-02-13 16:46:48 +0700

    Add format issue description

---
+++ field.js
@@ -0,0 +1,185 @@
+export const mandatoryInfo = {
+    fields: [
+        {
+            "id": "PVTF_lADOAJQP7c4AbG6AzgRbQ_c",
+            "name": "Domain"
+        },
+        {
+            "id": "PVTSSF_lADOAJQP7c4AbG6AzgRbQrI",
+            "name": "Status",
+            "options": [
+                {
+                    "id": "3e65d961",
+                    "name": "Needs Scoping"
+                },
+                {
+                    "id": "777fb8d8",
+                    "name": "On Hold"
+                },
+                {
+                    "id": "7a30089c",
+                    "name": "Scoping"
+                },
+                {
+                    "id": "f75ad846",
+                    "name": "Ready Todo"
+                },
+                {
+                    "id": "47fc9ee4",
+                    "name": "In Progress"
+                },
+                {
+                    "id": "9bd2299a",
+                    "name": "Blocked"
+                },
+                {
+                    "id": "45efc9ec",
+                    "name": "Review"
+                },
+                {
+                    "id": "0f8428d9",
+                    "name": "Testing"
+                },
+                {
+                    "id": "98236657",
+                    "name": "Done"
+                },
+                {
+                    "id": "1fe7aea4",
+                    "name": "Won't Do"
+                }
+            ]
+        },
+        {
+            "id": "PVTSSF_lADOAJQP7c4AbG6AzgRbSAQ",
+            "name": "Impact Type",
+            "options": [
+                {
+                    "id": "7dc7fbc4",
+                    "name": "Bug Fix"
+                },
+                {
+                    "id": "273e42cd",
+                    "name": "Observability"
+                },
+                {
+                    "id": "a92752a4",
+                    "name": "Maintainability"
+                },
+                {
+                    "id": "e82ba56c",
+                    "name": "Reliability"
+                },
+                {
+                    "id": "92ee6cab",
+                    "name": "Performance"
+                },
+                {
+                    "id": "6733947d",
+                    "name": "Toil"
+                },
+                {
+                    "id": "df94df03",
+                    "name": "Cost"
+                },
+                {
+                    "id": "57e97d9c",
+                    "name": "Functional"
+                },
+                {
+                    "id": "2a4a34dc",
+                    "name": "Security"
+                },
+                {
+                    "id": "e9c13393",
+                    "name": "Housekeeping"
+                },
+                {
+                    "id": "4ac02758",
+                    "name": "Software Architecture"
+                },
+                {
+                    "id": "1b13ca28",
+                    "name": "Compliance"
+                }
+            ]
+        },
+        {
+            "id": "PVTSSF_lADOAJQP7c4AbG6AzgSNrKg",
+            "name": "Priority",
+            "options": [
+                {
+                    "id": "e1ef926d",
+                    "name": "P1"
+                },
+                {
+                    "id": "718c4128",
+                    "name": "P2"
+                },
+                {
+                    "id": "e6046528",
+                    "name": "P3"
+                }
+            ]
+        },
+        {
+            "id": "PVTSSF_lADOAJQP7c4AbG6AzgUP77g",
+            "name": "Urgency",
+            "options": [
+                {
+                    "id": "bc21a226",
+                    "name": "Low"
+                },
+                {
+                    "id": "a9aa549e",
+                    "name": "Medium"
+                },
+                {
+                    "id": "dabc5638",
+                    "name": "High"
+                }
+            ]
+        },
+        {
+            "id": "PVTSSF_lADOAJQP7c4AbG6AzgUPM6g",
+            "name": "Effort",
+            "options": [
+                {
+                    "id": "43128794",
+                    "name": "Low"
+                },
+                {
+                    "id": "39436ebf",
+                    "name": "Medium"
+                },
+                {
+                    "id": "335b26eb",
+                    "name": "High"
+                },
+                {
+                    "id": "79dda3e4",
+                    "name": "1 day"
+                },
+                {
+                    "id": "cd650049",
+                    "name": "2-3 days"
+                },
+                {
+                    "id": "7c2aceef",
+                    "name": "3-5 days"
+                },
+                {
+                    "id": "de9d77d8",
+                    "name": "a few hours"
+                }
+            ]
+        },
+        {
+            "id": "PVTF_lADOAJQP7c4AbG6AzgSNreQ",
+            "name": "Start Date"
+        }
+    ],
+    project: {
+        id: process.env.PROJECT_ID,
+    }
+}
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-require('dotenv').config();
+import 'dotenv/config';
 import { Octokit } from "@octokit/core";
 import open from 'open';
 import XLSX from 'xlsx';
-
-const DECISION = {
-    DELETED: 'Deleted',
-    INTEGRATION_HANDLED: 'Integration Handled',
-}
+import { mandatoryInfo } from './field.js';
 
 const STATUS = {
     DONE: 'Done',
     IN_PROGRESS: 'In Progress',
-    TO_DO: 'To Do',
+    TO_DO: 'Ready Todo',
 }
 
 function readAndMergeDescription(filePath) {
@@ -43,27 +39,242 @@ async function openSSOUrl(error, callback) {
     await callback;
 }
 
-async function createIssues({ owner, repo, title, body, assignes }) {
+async function getIssues(owner, repo, issueNumber) {
+    const query = `
+    query($owner: String!, $repo: String!, $issueNumber: Int!) {
+        repository(owner: $owner, name: $repo) {
+            issue(number: $issueNumber) {
+                id
+                title
+                body
+                state
+                labels(first: 10) {
+                    nodes {
+                        id
+                        name
+                    }
+                }
+                projectItems(first: 10) {
+                    nodes {
+                        id
+                        project {
+                            id
+                            title
+                        }
+                        fieldValues(first: 100) {
+                            nodes {
+                                ... on ProjectV2ItemFieldTextValue {
+                                    field {
+                                        ... on ProjectV2FieldCommon {
+                                            id
+                                            name
+                                        }
+                                    }
+                                    text
+                                }
+                                ... on ProjectV2ItemFieldNumberValue {
+                                    field {
+                                        ... on ProjectV2FieldCommon {
+                                            id
+                                            name
+                                        }
+                                    }
+                                    number
+                                }
+                                ... on ProjectV2ItemFieldSingleSelectValue {
+                                    field {
+                                        ... on ProjectV2SingleSelectField {
+                                            id
+                                            name
+                                            options {
+                                                id
+                                                name
+                                            }
+                                        }
+                                    }
+                                    name
+                                    optionId
+                                }
+                                ... on ProjectV2ItemFieldDateValue {
+                                    field {
+                                        ... on ProjectV2FieldCommon {
+                                            id
+                                            name
+                                        }
+                                    }
+                                    date
+                                }
+                                ... on ProjectV2ItemFieldIterationValue {
+                                    field {
+                                        ... on ProjectV2FieldCommon {
+                                            id
+                                            name
+                                        }
+                                    }
+                                    title
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }`;
+
+    const variables = {
+        owner,
+        repo,
+        issueNumber
+    };
+
+    try {
+        const response = await octokit.graphql(query, variables);
+        return response.repository.issue;
+    } catch (error) {
+        if (error.response?.status === 403) {
+            await openSSOUrl(error, getIssues(owner, repo, issueNumber));
+        } else {
+            console.error('Error fetching issue:', error);
+        }
+    }
+}
+
+async function updateProjectV2ItemField(projectId, itemId, key, value) {
+    let newValue;
+    const fieldSelector = mandatoryInfo.fields.find(data => data.name === key)
+
+    if (!fieldSelector) {
+        throw new Error('Field doesn\'t exists');
+    }
+
+    if (fieldSelector && fieldSelector.options) {
+        const option = fieldSelector.options.find(option => option.name === value);
+        if (!option) {
+            throw new Error(`Option '${value}' not found for field '${key}'`);
+        }
+        newValue = { singleSelectOptionId: option.id }
+    
+    } else if (key === 'Start Date') {
+         // Handle date fields - ensure date is in ISO 8601 format
+         const date = new Date(value);
+         newValue = { date: date.toISOString() };
+    } else {
+        newValue = { text: value }
+    }
+
+    const mutation = `
+    mutation($projectId: ID!, $itemId: ID!, $fieldId: ID!, $value: ProjectV2FieldValue!) {
+        updateProjectV2ItemFieldValue(
+            input: {
+                projectId: $projectId,
+                itemId: $itemId,
+                fieldId: $fieldId,
+                value: $value
+            }
+        ) {
+            projectV2Item {
+                id
+            }
+        }
+    }`;
+
+    const variables = {
+        projectId,
+        itemId,
+        fieldId: fieldSelector.id,
+        value: newValue
+    };
+
+    let response;
+    try {
+        response = await octokit.graphql(mutation, variables);
+    } catch (error) {
+        console.error('Error updating project field:', error);
+        if (error.response?.status === 403) {
+            await openSSOUrl(error, () => updateProjectV2ItemField(projectId, itemId, fieldId, value));
+        } else {
+            console.log(error);
+        }
+    }
+    return response;
+}
+
+async function assignProjectToIssue(projectId, contentId) {
+    const mutation = `
+    mutation($projectId: ID!, $contentId: ID!) {
+        addProjectV2ItemById(input: {projectId: $projectId, contentId: $contentId}) {
+            item {
+                id
+            }
+        }
+    }
+    `;
+
+    const variables = {
+        projectId,
+        contentId
+    };
+
+    let response;
+    try {
+        response = await octokit.graphql(mutation, variables);
+    } catch (error) {
+        console.error('Error assigning issue to project:', error);
+        if (error.response.status && error.response.status === 403) {
+            await openSSOUrl(error, assignProjectToIssue(projectId, contentId));
+        } else {
+            console.log(error);
+        }
+    }
+
+    return response;
+}
+
+async function createIssues({ owner, repo, title, body, assignes, description }) {
     try {
         const newAssignes = [];
-        const payload = {
+        const parent = {
             owner,
             repo,
             title,
             body,
         }
 
+        const { data } = await octokit.request(`POST /repos/{owner}/{repo}/issues`, parent);
+
         if (assignes && Array.isArray(assignes) && assignes.length > 0) {
             newAssignes.push(...assignes);
+
+            await octokit.request(`POST /repos/{owner}/{repo}/issues/{issue_number}/assignees`, {
+                owner,
+                repo,
+                issue_number: data.number,
+                assignees: newAssignes
+            });
         }
 
-        const { data } = await octokit.request(`POST /repos/{owner}/{repo}/issues`, payload);
+        const response = await getIssues(owner, repo, data.number);
 
-        console.log(data);
-        console.log('Issue created successfully');
+        const { addProjectV2ItemById: { item: { id } } } = await assignProjectToIssue(mandatoryInfo.project.id, response.id);
+
+        const payload = {
+            'Domain': description['Domain'],
+            'Status': STATUS.TO_DO,
+            'Impact Type': description['Impact Type'],
+            'Priority': description['Priority'],
+            'Urgency': description['Urgency'],
+            'Effort': description['Effort'],
+            'Start Date': description['Start Date'] ? description['Start Date'] : new Date().toISOString()
+        }
+
+        for (const [key, value] of Object.entries(payload)) {
+            await updateProjectV2ItemField(mandatoryInfo.project.id, id, key, value)
+        }
+        console.log('Issue created successfully: ', data.html_url);
 
         return data;
     } catch (error) {
+        console.error('Error creating issue:', error);
         if (error.response.status && error.response.status === 403) {
             await openSSOUrl(error, createIssues({ owner, repo, title, body }));
         } else {
@@ -73,40 +284,39 @@ async function createIssues({ owner, repo, title, body, assignes }) {
 
 }
 
-async function getRepo(owner, repo) {
-    try {
-        const { data } = await octokit.request(`GET /repos/{owner}/{repo}`, {
-            owner,
-            repo,
-        });
-
-        return data;
-    } catch (error) {
-        if (error.response.status && error.response.status === 403) {
-            await openSSOUrl(error, getRepo(owner, repo));
-        } else {
-            console.log(error);
-        }
-    }
-}
-
 async function convertToIssues(issues) {
     for (const issue of issues) {
         const status = issue.status;
-        const decision = issue.decision;
 
         if (status === STATUS.DONE) {
             continue;
         }
 
-        if (status === STATUS.TO_DO && decision === DECISION.INTEGRATION_HANDLED) {
-            await createIssues({
+        if (status === STATUS.TO_DO) {
+           const request = {
                 owner: issue.owner,
                 repo: issue.repo,
                 title: issue.title,
                 body: issue.body,
-                assignes: [] // TBD for assignes
-            });
+                description: {
+                    'Domain': issue['Domain'],
+                    'Status': status,
+                    'Impact Type': issue['Impact Type'],
+                    'Priority': issue['Priority'],
+                    'Urgency': issue['Urgency'],
+                    'Effort': issue['Effort'],
+                }
+            }
+
+            if (issue.assignees) {
+                request.assignes = issue.assignees.includes(',') ? issue.assignees.split(',') : [issue.assignees];
+            }
+
+            if (issue['Start Date'] && issue['Start Date'] !== '') {
+                request.description['Start Date'] = issue['Start Date'];
+            }
+
+            await createIssues(request);
         }
     }
 
